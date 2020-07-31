e8e82dbd9aefb6b69185e88d4d72dbf6ad6e8457
sameerdewan <sameer.dewan@itential.com>
2020-07-31 09:41:57 -0400

    fix sync

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,10 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
 const Web3 = require('web3');
 const FlightSuretyApp = artifacts.require("FlightSuretyApp");
 const FlightSuretyData = artifacts.require("FlightSuretyData");
 const fs = require('fs');
+const infuraKey = 'https://rinkeby.infura.io/v3/421d2d13751d477088f378ceb77dd58c';
+const mnemonic = 'squirrel march broom strong kick blast unique team song song assist million';
 
 module.exports = function(deployer) {
 
@@ -22,14 +25,14 @@ module.exports = function(deployer) {
                             }
                         }
                     };
-                    fs.writeFileSync(__dirname + '/deployments.json',JSON.stringify(config, null, '\t'), 'utf-8');
-                    const web3 = new Web3(config.localhost.url);
+                    fs.writeFileSync(__dirname + '../deployments.json',JSON.stringify(config, null, '\t'), 'utf-8');
+                    const provider = new HDWalletProvider(mnemonic, infuraKey);
+                    const web3 = new Web3(provider);
                     const { abi, address } = config.localhost.FlightSuretyData;
                     const dataContractinstance = new web3.eth.Contract(abi, address);
                     const { address: appAddress } = config.localhost.FlightSuretyApp; 
                     dataContractinstance.methods.wireApp(appAddress).call()
                         .then(result => {
-                            console.log({result});
                             console.log('<--WIRED APP-->');
                         }).catch(error => {
                             console.log({error});

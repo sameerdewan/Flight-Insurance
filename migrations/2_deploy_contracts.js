52b747489900561b51424c857e8a7fc46c901bd1
sameerdewan <sameer.dewan@itential.com>
2020-07-31 08:36:24 -0400

    lock

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -14,7 +14,7 @@ module.exports = function(deployer) {
                             dataAddress: FlightSuretyData.address,
                             appAddress: FlightSuretyApp.address
                         }
-                    }
+                    };
                     fs.writeFileSync(__dirname + '/../src/dapp/config.json',JSON.stringify(config, null, '\t'), 'utf-8');
                     fs.writeFileSync(__dirname + '/../src/server/config.json',JSON.stringify(config, null, '\t'), 'utf-8');
                 });

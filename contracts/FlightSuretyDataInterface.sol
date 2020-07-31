c8d43d44f3eaa4a01223d8787d90ff637aeee9ed
sameerdewan <sameer.dewan@itential.com>
2020-07-31 04:48:21 -0400

    interface

--- contracts/FlightSuretyDataInterface.sol
+++ contracts/FlightSuretyDataInterface.sol
@@ -1,20 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.21 <0.7.0;
 
-abstract contract FlightSuretyData {
+interface FlightSuretyData {
     // Contract Owner Functions
-    function disableContract(address _address) external virtual;
-    function enableContract(address _address) external virtual;
+    function disableContract(address _address) external;
+    function enableContract(address _address) external;
 
     // Utilities
-    function getInsuredStatus(string memory _airline) external virtual returns (bool airlineIsFunded);
+    function getInsuredStatus(string memory _airline) external returns (bool airlineIsFunded);
 
     // Airline Functions
-    function applyAirline(address _address, string memory _name) external virtual;
-    function voteAirline(address _address, address _voter, string memory _name) external virtual;
-    function fundAirline(address _funder, address _airline, uint _funds) external virtual;
-    function addFlight(string memory _flight, address _caller, address _airline) external virtual;
+    function applyAirline(address _address, string memory _name) external;
+    function voteAirline(address _address, address _voter, string memory _name) external;
+    function fundAirline(address _funder, address _airline, uint _funds) external;
+    function addFlight(string memory _flight, address _caller, address _airline) external;
 
     // Passenger Functions
-    function buyInsurance(address _passenger, string memory _airline, string memory _flight, uint _funds) external virtual payable;
+    function buyInsurance(address _passenger, string memory _airline, string memory _flight, uint _funds) external payable;
 }

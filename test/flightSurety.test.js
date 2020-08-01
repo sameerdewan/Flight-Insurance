1a0870b5580852436344260d4945f90d8c8c838a
sameerdewan <sameer.dewan@itential.com>
2020-08-01 11:42:51 -0400

    refactor

--- test/flightSurety.test.js
+++ test/flightSurety.test.js
@@ -10,8 +10,7 @@ const third_airline_name = "THIRD_TEST_AIRLINE";
 const fourth_airline_name = "FOURTH_TEST_AIRLINE";
 const fifth_airline_name = "FIFTH_TEST_AIRLINE";
 
-const first_flight = "FIRST_TEST_FLIGHT";
-const second_flight = "SECOND_TEST_FLIGHT";
+const default_initial_flight = "FIRST_TEST_FLIGHT";
 
 let accounts;
 
@@ -212,12 +211,16 @@ it('airline 2 should be able to add a flight', async () => {
     const _timeOfFlight = new Date(2021, 00, 01, 10, 30, 00, 0) // January 1, 2021 10:30
     const _timeOfFlightInSeconds = _timeOfFlight.getTime() / 1000;
     await dataContract.FlightAdded(() => airline_flight_added_event_emitted = true);
-    await appContract.addFlight.sendTransaction(first_flight, secondAirline, _timeOfFlightInSeconds, { from: secondAirline, gas: default_gas });
-    const { name, airline, timeOfFlightInSeconds } = await dataContract.getFlight.call(second_airline_name, first_flight, { from: owner });
-    const expectedFlightState = [first_flight, secondAirline, _timeOfFlightInSeconds];
+    await appContract.addFlight.sendTransaction(default_initial_flight, secondAirline, _timeOfFlightInSeconds, 
+        { from: secondAirline, gas: default_gas }
+    );
+    const { name, airline, timeOfFlightInSeconds } = await dataContract.getFlight.call(second_airline_name, default_initial_flight, { from: owner });
+    const expectedFlightState = [default_initial_flight, secondAirline, _timeOfFlightInSeconds];
     const actualFlightState = [name, airline, Number(`${timeOfFlightInSeconds}`)];
     const error1 = "Error: Unexpected Flight State";
     assert.deepEqual(expectedFlightState, actualFlightState, error1);
     const error2 = "Error: Flight Added Event not Emitted";
     assert.equal(airline_flight_added_event_emitted, true, error2);
 });
+
+it('passenger should be able to buy insurance for airline 2 flight');

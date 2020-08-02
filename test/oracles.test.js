e6472fe2024e2526a11af3035dc5bad1e09dc36c
sameerdewan <sameer.dewan@itential.com>
2020-08-02 12:32:39 -0400

    oracles can register

--- test/oracles.test.js
+++ test/oracles.test.js
@@ -4,9 +4,11 @@ const FlightSuretyApp = artifacts.require("FlightSuretyApp");
 
 describe('Oracle Tests', () => {
     const default_gas = 9500000;
-    const default_fee = web3.utils.toWei("1");
+    const default_oracle_fee = web3.utils.toWei("1");
+    const default_minimum_funding = web3.utils.toWei("10");
     const default_initial_airline_name = "INITIAL_TEST_FLIGHT";
     const default_initial_flight = "FIRST_TEST_FLIGHT";
+    const default_minimum_oracles = 30;
 
     let accounts;
 
@@ -14,22 +16,30 @@ describe('Oracle Tests', () => {
     let dataContract;
     let appContract;
 
-    let firstAirline;
-
-    let passenger;
-
     contract('Oracle Tests', async (acc) => {
         accounts = acc;
         owner = accounts[0];
-        firstAirline = accounts[0];
-        passenger = accounts[1];
     });
     
     before(async () => {
-        dataContract = await FlightSuretyData.new(initial_airline_name, { from: owner, value: default_minimum_funding, gas: default_gas });
+        dataContract = await FlightSuretyData.new(default_initial_airline_name, { from: owner, value: default_minimum_funding, gas: default_gas });
         appContract = await FlightSuretyApp.new(dataContract.address, { from: owner, gas: default_gas });
         await dataContract.wireApp.sendTransaction(appContract.address, { from: owner });
     });
 
-    
+    it('oracles can register', async () => {
+        let oracleCount = 0;
+        const registrations = [];
+        while (oracleCount < default_minimum_oracles) {
+            const currentOracle = accounts[oracleCount];
+            registrations.push(appContract.registerOracle.sendTransaction({ from: currentOracle, value: default_oracle_fee }));
+            oracleCount+=1;
+        }
+        await Promise.all(registrations).then(txs => {
+            for (let index = 0; index < txs.length; index++) {
+                const tx = txs[index];
+                truffleAssert.eventEmitted(tx, 'OracleRegistered');
+            }
+        });
+    });
 });

e81ebda913e15eda2bf5362cde032795de959080
sameerdewan <sameer.dewan@itential.com>
2020-08-02 10:42:49 -0400

    stub

--- server/index.js
+++ server/index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const Web3 = require('web3');
 const app = express();
+const { FlightSuretyApp, FlightSuretyData, url } = require('./deployments.json').localhost;
+const refinedURL = url.replace('http', 'ws');
 
-let oracle_history = [];
+const oracle_history = [];
+const oracles = {};
 
+/* WEB3 APP */
+
+
+/* SERVER APP */
 app.get('/api', (_, res) => {
     res.json({ oracle_history });
 });

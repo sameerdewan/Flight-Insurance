6eb12dc8935915fbd218ff01895006e1a1384a77
sameerdewan <sameer.dewan@itential.com>
2020-09-28 02:42:43 -0400

    styles

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,6 +2,12 @@ import React, { useContext } from 'react';
 import Web3Context from '../contexts/Web3'; 
 import MetamaskLogo from '../images/metamask_logo.png';
 import { Navbar } from 'react-bootstrap'; 
+import styled from 'styled-components';
+
+const AccountCode = styled.code`
+    color: white;
+    font-size: 14px;
+`;
 
 export default function Footer() {
     const { account } = useContext(Web3Context);
@@ -15,7 +21,7 @@ export default function Footer() {
                 height="30"
                 className="d-inline-block align-top"
                 />{' '}
-                {account}
+                <AccountCode>{account}</AccountCode>
             </Navbar.Brand>
         </Navbar>
     );

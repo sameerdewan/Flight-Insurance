64a597c760d1d3ec9234de150846b66b1129e5a2
sameerdewan <sameer.dewan@itential.com>
2020-10-10 18:35:10 -0400

    useHashRouter

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Nav } from 'react-bootstrap';
 import { HOME, FLIGHTS, ADMIN } from '../router/routerPaths';
 import { changePath } from '../utils/routerFunctions';
+import { upperFirst } from 'lodash';
 
 const TABS = [
     {
@@ -26,9 +27,34 @@ function Tab({ tab }) {
     );
 }
 
+const useHashRouter = () => {
+    const [hash, setHash] = React.useState(window.location.hash);
+    const listenToPopstate = () => {
+      const winHash = window.location.hash;
+      setHash(winHash);
+    };
+    React.useEffect(() => {
+      window.addEventListener("popstate", listenToPopstate);
+      return () => {
+        window.removeEventListener("popstate", listenToPopstate);
+      };
+    }, []);
+    return hash;
+  };
+
 export default function Navigation() {
+    const [label, setLabel] = useState(TABS[0].label);
+    const hash = useHashRouter();
+    useEffect(() => {
+        let _label = upperFirst(hash.replace('#/', ''));
+        if (_label === "") {
+            _label = TABS[0].label;
+        }
+        setLabel(_label);
+    }, [hash]);
+
     return (
-        <Nav variant="tabs" defaultActiveKey={TABS[0].label}>
+        <Nav variant="tabs" defaultActiveKey={TABS[0].label} activeKey={label}>
           { TABS.map(tab => <Tab tab={tab} key={tab.label} />) }
         </Nav>
     );

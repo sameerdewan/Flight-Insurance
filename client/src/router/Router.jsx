46cf7efd1c09b34b353e55916df2fc7d140098a7
sameerdewan <sameer.dewan@itential.com>
2020-10-14 04:38:15 -0400

    fianlize

--- client/src/router/Router.jsx
+++ client/src/router/Router.jsx
@@ -24,8 +24,8 @@ export default function Router() {
                                         Note: These URLs will not work until <code>npm run server</code> has been ran, which
                                         must be run <b>after</b> initializing all contracts under the <b>Contract Administration</b> tab.
                                     </Card.Text>
-                                    <Card.Link href="http://localhost:5000/oracles">View All Oracles</Card.Link>
-                                    <Card.Link href="http://localhost:5000/logs">View Oracle Logs</Card.Link>
+                                    <Card.Link target="_blank" href="http://localhost:5000/oracles">View All Oracles</Card.Link>
+                                    <Card.Link target="_blank" href="http://localhost:5000/logs">View Oracle Logs</Card.Link>
                                     <center><img src={Oracle} height={250} width={250} /></center>
                                 </Card.Body>
                             </Card>
@@ -36,6 +36,17 @@ export default function Router() {
                                     <Card.Text>
                                         Watch a short guided video which explains how to set up and interact with this <b>dApp</b>.
                                     </Card.Text>
+                                    <Card.Link target="_blank" href="https://www.youtube.com/watch?v=Ei4pDGOKuUg">1. Setting up the Project</Card.Link>
+                                    <br />
+                                    <Card.Link target="_blank" href="https://www.youtube.com/watch?v=qArOa3MNDko">2. Starting the dApp</Card.Link>
+                                    <br/>
+                                    <Card.Link target="_blank" href="https://www.youtube.com/watch?v=sQF9RQkqmfA">3. Airline Administration</Card.Link>
+                                    <br />
+                                    <Card.Link target="_blank" href="https://www.youtube.com/watch?v=dYen0hV3V8g">4. Airline Funding and Flight Creation</Card.Link>
+                                    <br />
+                                    <Card.Link target="_blank" href="https://www.youtube.com/watch?v=ed67CiWAS3g">5. Running our Oracles</Card.Link>
+                                    <br />
+                                    <Card.Link target="_blank" href="https://www.youtube.com/watch?v=0p-yBrRm7Y8">6. Getting an Insurance Payout</Card.Link>
                                 </Card.Body>
                             </Card>
                         </div>
